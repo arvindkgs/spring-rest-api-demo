9e5e0c0a0ce4de6d62fa81fbbb62328b085a039c
arvindkgs <mail.arvind85@gmai.com>
2020-04-24 16:06:48 +0530

    Remove unnecessary imports

--- src/test/java/com/spring/rest_api_demo/SpringRestApiDemoApplicationTests.java
+++ src/test/java/com/spring/rest_api_demo/SpringRestApiDemoApplicationTests.java
@@ -1,18 +1,11 @@
 package com.spring.rest_api_demo;
 
-import com.spring.rest_api_demo.configuration.WebSecurityConfiguration;
-import com.spring.rest_api_demo.controller.ApiController;
-import com.spring.rest_api_demo.service.CalculateService;
-import com.spring.rest_api_demo.service.CalculateServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
