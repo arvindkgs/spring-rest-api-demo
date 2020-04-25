4ac62b42c24fac988467029ebfb00180da33114c
arvindkgs <mail.arvind85@gmai.com>
2020-04-25 19:32:07 +0530

    Handling different error types

--- src/main/java/com/spring/rest_api_demo/configuration/CustomExceptionHandler.java
+++ src/main/java/com/spring/rest_api_demo/configuration/CustomExceptionHandler.java
@@ -1,17 +1,28 @@
 package com.spring.rest_api_demo.configuration;
 
+import org.springframework.boot.json.JsonParseException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+    
+    @ExceptionHandler(JsonParseException.class)
+    public ResponseEntity<Object> handleJsonParseException(JsonParseException ex) throws IOException {
+        return new ResponseEntity<Object>("Invalid input", HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(NumberFormatException.class)
-    public void handleAPINumberFormatException(Exception ex, HttpServletResponse response) throws IOException {
-        response.sendError(400, "Only numbers are can be passed as input.");
+    public ResponseEntity<Object> handleNumberFormatException(NumberFormatException ex) throws IOException {
+        return new ResponseEntity<Object>("Only numbers allowed", HttpStatus.BAD_REQUEST);
     }
+
 }
--- src/main/java/com/spring/rest_api_demo/controller/ApiController.java
+++ src/main/java/com/spring/rest_api_demo/controller/ApiController.java
@@ -4,6 +4,7 @@ import com.spring.rest_api_demo.model.Input;
 import com.spring.rest_api_demo.model.Result;
 import com.spring.rest_api_demo.service.CalculateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JsonParseException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,8 @@ public class ApiController {
 
     @PostMapping("/add")
     public Result add(@RequestBody Input input){
+        if(input == null || input.getX() == null || input.getY() == null)
+            throw new JsonParseException();
         int x = Integer.parseInt(input.getX());
         int y = Integer.parseInt(input.getY());
         return new Result(calculateService.add(x,y) +"");
